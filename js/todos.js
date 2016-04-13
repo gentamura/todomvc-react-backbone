da6960b27909d3ccbf0647839d7b3b7203e40a2b
Gen Tamura <gen.tamura84@gmail.com>
2016-04-13 17:49:06 +0900

    First commit

---
+++ js/todos.js
@@ -0,0 +1,31 @@
+var app = app || {};
+
+(function() {
+  'use strict';
+
+  var Todos = Backbone.Collection.extend({
+
+    model: app.Todo,
+
+    localStorage: new Backbone.LocalStorage('todomvc-react-backbone'),
+
+    completed: function() {
+      return this.where({ completed: true });
+    },
+
+    remaining: function() {
+      return this.where({ completed: false });            
+    },
+
+    nextOrder: function() {
+      return this.length ? this.last().get('order') + 1 : 1;
+    },
+
+    comparator: 'order'
+
+  });
+
+
+  app.todos = new Todos();
+
+})();
